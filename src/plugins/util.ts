dd28085857c0c0c5ae53d4cd25efaf14c3349552
firecraftgaming <eliyah@sundstroem.com>
2024-03-04 21:17:05 +0100

    chore: removed internal plugins for release candidate

--- src/plugins/util.ts
+++ src/plugins/util.ts
@@ -6,14 +6,14 @@ import {CasparPlugin} from '@lappis/cg-manager';
 
 function loadPluginFolderUnsafe(dir: string) {
     return fs.readdirSync(dir)
-        .filter((file) => !file.includes('.'))
+        .filter((file) => !file.includes('.')) // TODO: better way to check if folder
         .map((file) => require(path.join(dir, file)).default as typeof CasparPlugin);
 }
 
 export function loadPluginFolder(dir: string) {
     const [err, files] = noTry(() => loadPluginFolderUnsafe(dir));
     if (err) {
-        if (!err.message.includes('ENOENT')) Logger.error(`Error loading plugins from ${dir}: ${err}`);
+        if (!err.message.includes('ENOENT')) Logger.error(`Error loading plugins from ${dir}: ${err}`); // TODO: better way to check for ENOENT code
         return [];
     }
 
